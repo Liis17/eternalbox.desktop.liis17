7ebe46333229d19e73dbbfab84540ce4c1b17a71
Li_is <daskent96@gmail.com>
2021-04-02 15:05:50 +0300

    +

---
+++ eternalbox.desktop.liis17/DebugWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace eternalbox.desktop.liis17
+{
+    /// <summary>
+    /// Логика взаимодействия для DebugWindow.xaml
+    /// </summary>
+    public partial class DebugWindow : Window
+    {
+        public static int WidthWindow = 0;
+        public static int HeightWindow = 0;
+
+        public DebugWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
