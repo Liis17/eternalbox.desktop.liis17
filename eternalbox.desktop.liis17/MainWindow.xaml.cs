8f9ab5d3abd60a23902f8d85c92150bd91e41ba8
Li_is <daskent96@gmail.com>
2021-04-02 12:57:28 +0300

    alpha 1 rc

--- eternalbox.desktop.liis17/MainWindow.xaml.cs
+++ eternalbox.desktop.liis17/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using DiscordRPC;
+using DiscordRPC.Logging;
 
 namespace eternalbox.desktop.liis17
 {
@@ -22,9 +24,59 @@ namespace eternalbox.desktop.liis17
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DiscordRpcClient client;
+
         public MainWindow()
         {
             InitializeComponent();
+			Initialize();
+
+		}
+
+		void Initialize()
+		{
+			/*
+			Create a Discord client
+			NOTE: 	If you are using Unity3D, you must use the full constructor and define
+					 the pipe connection.
+			*/
+			client = new DiscordRpcClient("827472021759197194");
+
+			//Set the logger
+			client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+
+			//Subscribe to events
+			client.OnReady += (sender, e) =>
+			{
+				Console.WriteLine("Received Ready from user {0}", e.User.Username);
+			};
+
+			client.OnPresenceUpdate += (sender, e) =>
+			{
+				Console.WriteLine("Received Update! {0}", e.Presence);
+			};
+
+			//Connect to the RPC
+			client.Initialize();
+
+			//Set the rich presence
+			//Call this as many times as you want and anywhere in your code.
+			client.SetPresence(new RichPresence()
+			{
+				Details = "Listens to Eternal Jukebox",
+				State = "...",
+				Assets = new Assets()
+				{
+					LargeImageKey = "eternal",
+					LargeImageText = "The Eternal Jukebox",
+					SmallImageKey = "image_small"
+				}
+			});
+		}
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+			client.Dispose();
         }
     }
 }
